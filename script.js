bc18824e1137bdc90133a8091e0a7657d8b47dd0
Melanie-J-Baker <baker_mel@hotmail.com>
2022-09-08 09:45:30 +0100

    Add Javascript

---
+++ script.js
@@ -0,0 +1,22 @@
+const choices = ["rock", "paper", "scissors"];
+
+function game() {
+  playRound();
+}
+
+function playRound() {
+  const playerSelection = playerChoice();
+  const computerSelection = computerChoice();
+}
+
+function playerChoice() {
+  let input = prompt('Type Rock, Paper or Scissors');
+  input = input.toLowerCase();
+  console.log(input);
+}
+
+function computerChoice() {
+  return choices[Math.floor(Math.random()*choices.length)]
+}
+
+game();
